57d7a13f73af89b0f572a9f03096f3f844840ecd
dwangster448 <dwang448@wisc.edu>
2025-02-28 15:28:15 -0600

    small confirmation functions edits

--- cl_button_maker/app.js
+++ cl_button_maker/app.js
@@ -26,19 +26,19 @@ let user_phone_number = "2624120000"
 let startDate = "06/01/2026" // current date of reservation
 let endDate = "06/04/2026" // end date that will be adjusted in reservation
 
-let available = false
+let available = true;
 
 function disclaimer(reservation_data=False) {
     // popup modal that provides user information, reservation dates, and disclaimer notes
-    console.log("Disclaimer")
+    console.log("Disclaimer", reservation_data)
 }
 
-async function submit_reservation(user_information) {
+function submit_reservation(user_information) {
     console.log("submit function")
 
-    collection = "reservation_collection";
+    let collection = "reservation_collection";
 
-    return False; //TODO Placeholder to prevent return errors
+    return true; //TODO Placeholder to prevent return errors
 
     
     // add user information passed onto firebase
@@ -50,7 +50,7 @@ async function submit_reservation(user_information) {
     //     // Add a new document with a generated ID
     //     const docRef = await addDoc(collection(db, {collection}), user_information);
     //     console.log("Document written with ID: ", docRef.id);
-    //     return True;
+    //     return true;
     // } catch(error) {
     //   console.error("Error pushing data: ", error);
     //   return False;
@@ -59,20 +59,22 @@ async function submit_reservation(user_information) {
 
 }
 
-function available_dates(start=False, end=False) {
+function available_dates(start=false, end=false) {
+    console.log("availla")
     // if any of the requested dates are not avaialble, return false
 
-    if (start || end == False) { //Missing dates needed to confirm availability
+    if ((start || end) == false) { //Missing dates needed to confirm availability
         return false;
     }
 
-    let overlapped_dates = True; //TODO add functionality to check overlapping dates
+    let overlapped_dates = true; //TODO add functionality to check overlapping dates
     if (overlapped_dates) { // if true = requested dates not available
         return false;     
     }
 }
 
-async function confirmation() {
+function confirmation() {
+    console.log("confirmation detected")
     
     // confirmation pop-up
 
@@ -80,8 +82,10 @@ async function confirmation() {
         console.log("Overlap dates detected");
         return;
     } 
+
+    console.log("herer")
     
-    const reservation_data = { //JSON to store user data to add onto firebase
+    let reservation_data = { //JSON to store user data to add onto firebase
         "firstName":first_name,
         "lastName":last_name,
         "email":user_email,
@@ -90,11 +94,12 @@ async function confirmation() {
         "endDate":endDate,
     };
 
+    console.log("----")
     
     if (available) {
         try {
             // submit reservation
-            const submitation_result = submit_reservation(reservation_data);
+            let submitation_result = submit_reservation(reservation_data);
             if (submitation_result) {
                 // add disclaimers via disclaimer function
                 disclaimer(reservation_data);
